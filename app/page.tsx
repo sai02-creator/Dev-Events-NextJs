87f993122847203de9991ea3394194f5bbe1038a
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-12-20 00:06:29 +1100

    Add date and time icons with event details

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
+import events from "@/lib/constants"
+
+
+
 
 const Page = () => {
   return (
@@ -10,9 +15,12 @@ const Page = () => {
   <div className="mt-20 space-y-7">
     <h3> Featured Events</h3>
     <ul className="events">
-      {[1,2,3,4,5].map((event) => (
-        <li key={event}>Event {event}</li>
-      ))}
+      {events.map((event) => (
+        <li key={event.title}>
+         <EventCard {...event} />
+        </li>
+
+    ))}
     </ul>
 
   </div>
