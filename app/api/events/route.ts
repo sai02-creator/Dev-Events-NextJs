63c53e87234c9122f93ae0edd77cfe5816277792
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-12-23 01:44:32 +1100

    feat: return 400 response for invalid JSON data in POST endpoint

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -12,10 +12,10 @@ export async function POST(req: NextRequest) {
       try {
         event = Object.fromEntries(formData.entries());
       } catch (e){
-
+          return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
       }
     } catch(e) {
         console.error(e);
-        return NextResponse.json({ message: 'Event Creation Failed', error: e  instanceof Error ? e.message : 'Unknown'})
+        return NextResponse.json({ message: 'Event Creation Failed', error: e  instanceof Error ? e.message : 'Unknown'}, { status : 500 })
     }
 }
